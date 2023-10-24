29c36126f627a6c5b47081e845407589a7dd36c8
NestorAec26 <143562624+NestorAec26@users.noreply.github.com>
2023-10-24 16:54:13 -0700

    Add files via upload

---
+++ EVA2_12_CALIFAS_EUA/src/eva2_12_califas_eua/EVA2_12_CALIFAS_EUA.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_12_califas_eua;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_12_CALIFAS_EUA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int califa;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Introduce tu calificacion");
+        califa = input.nextInt();
+        if ((califa >-1)&&(califa<=100))
+            System.out.println("calIFICACION VALIDA");
+        if ((califa >=90)&&(califa<=100))
+            System.out.println("califa:" + "A");
+        else if ((califa >=80)&&(califa<=89))
+            System.out.println("califa:" + "B");
+        else if ((califa >=70)&&(califa<=79))
+            System.out.println("califa:" + "C");
+        else if ((califa >=60)&&(califa<=69))
+            System.out.println("califa:" + "D");
+        else if ((califa < 60))
+            System.out.println("califa:" + "F");
+        else
+            System.out.println("No es una calificacion aprobatoria");
+            
+    }
+    
+}
