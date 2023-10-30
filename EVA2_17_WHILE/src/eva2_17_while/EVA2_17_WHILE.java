d3916da7b647a27a50b918b4a3772d79417eeb56
NestorAec26 <143562624+NestorAec26@users.noreply.github.com>
2023-10-30 17:50:54 -0600

    Add files via upload

---
+++ EVA2_17_WHILE/src/eva2_17_while/EVA2_17_WHILE.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_17_while;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_17_WHILE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // EL CICLO WHILE SE EJECUTA EN CERO N VECES
+        int i =10;
+        i = 0;
+        while (i < 10){
+            System.out.println("Nestor!!");
+        i++;
+        }
+    }
+    
+}
