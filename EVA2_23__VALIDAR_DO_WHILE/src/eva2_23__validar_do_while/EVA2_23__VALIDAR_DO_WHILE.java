a27138bb10a8b1952e4facc18ff3ea8081452377
NestorAec26 <143562624+NestorAec26@users.noreply.github.com>
2023-11-01 17:54:47 -0600

    Add files via upload

---
+++ EVA2_23__VALIDAR_DO_WHILE/src/eva2_23__validar_do_while/EVA2_23__VALIDAR_DO_WHILE.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_23__validar_do_while;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_23__VALIDAR_DO_WHILE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int valor;
+        String cade;
+        cade.
+        Scanner input = new Scanner(System.in);
+        
+        do{
+            System.out.println("Introduce el valor entero");
+            valor = input.nextInt();
+        }while(true);
+    }
+    
+}
